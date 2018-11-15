170131e9b5a08bf4ff945b326f9a5a32d0bf5445
Stephane BACHELIER <stephane.bachelier@gmail.com>
2018-11-15 01:30:50 +0100

    test(r/hello/sync): add integration test

---
+++ test/hello.sync.test.js
@@ -0,0 +1,31 @@
+const test = require('ava')
+const got = require('got')
+
+const { baseUrl } = require('../options')
+
+const getLetters = string => {
+  const letters = []
+  for (let i in string) {
+    letters.push(string[i])
+  }
+
+  return letters.sort().join()
+}
+
+const DEFAULT_INPUT = 'hello world'
+const ITERATIONS = 20
+const DEFAULT_LETTERS = getLetters(DEFAULT_INPUT)
+
+test('should access GET /r/hello/sync', async t => {
+  const { statusCode, body } = await got.get(`${baseUrl}/r/hello/sync`, {
+    json: true
+  })
+
+  t.is(statusCode, 200)
+  t.is(body.length, ITERATIONS)
+
+  t.true(body.every(line => {
+    t.deepEqual(getLetters(line), DEFAULT_LETTERS)
+    return true
+  }))
+})
