be620fd602ca39463547fa41ee077748e9995370
Stephane BACHELIER <stephane.bachelier@gmail.com>
2018-11-15 01:55:15 +0100

    feat(bridge): add async example of sync call to R

--- r-bridge/index.js
+++ r-bridge/index.js
@@ -4,5 +4,19 @@ module.exports = {
   foo: (name = 'hello world', times = 20) =>
     R('r/sync.R')
       .data(name, times)
-      .callSync()
+      .callSync(),
+
+  bar: (name = 'hello world', times = 20) => {
+    return new Promise((resolve, reject) => {
+      R('r/sync.R')
+        .data(name, times)
+        .call((err, data) => {
+          if (err) {
+            return reject(err)
+          }
+
+          resolve(data)
+        })
+    })
+  }
 }
