3cc16aceff0a9d1bdc18acc628d9ee1061717a9c
Stephane BACHELIER <stephane.bachelier@gmail.com>
2018-11-15 02:06:25 +0100

    test(r/baz/async): add integration test

---
+++ test/baz.async.test.js
@@ -0,0 +1,24 @@
+const test = require('ava')
+const got = require('got')
+
+const { baseUrl } = require('../options')
+
+const getLetters = require('./helpers/letters')
+
+test('should access GET /r/baz/async', async t => {
+  const input = 'Lorem ipsum dolor sit amet'
+  const iterations = 42
+  const letters = getLetters(input)
+
+  const { statusCode, body } = await got.get(`${baseUrl}/r/baz/async/${input}/${iterations}`, {
+    json: true
+  })
+
+  t.is(statusCode, 200)
+  t.is(body.length, iterations)
+
+  t.true(body.every(line => {
+    t.deepEqual(getLetters(line), letters)
+    return true
+  }))
+})
